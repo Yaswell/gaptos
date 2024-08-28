1e4412776bcb750c5d35d07c8925a17e4ae2610d
Yaswell <featherstone113@gmail.com>
2024-08-28 10:28:07 -0400

    updating home

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
+import React from "react";
 import Home from "./Views/Home";
-import Nosotros from "./Views/Nosotros"
+import Nosotros from "./Views/Nosotros";
 import Transparencia from "./Views/Transparencia";
-import Donaciones from "./Views/Donaciones"
+import Donaciones from "./Views/Donaciones";
 import Galeria from "./Views/Galeria";
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-            
-    <Route path='/' element = {<Home />}  />
-    <Route path='/nosotros' element = {<Nosotros />}  />
-    <Route path='/transparencia' element = {<Transparencia />}  />
-    <Route path='/donaciones' element = {<Donaciones />}  />
-    <Route path='/galeria' element = {<Galeria />}  />
-
-
-
-  </Routes>
+      <Route path="/gaptos" element={<Home />} />
+      <Route path="/nosotros" element={<Nosotros />} />
+      <Route path="/transparencia" element={<Transparencia />} />
+      <Route path="/donaciones" element={<Donaciones />} />
+      <Route path="/galeria" element={<Galeria />} />
+    </Routes>
   );
 }
 
--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,29 +1,31 @@
-import React from 'react'
-import Header from '../components/header/header.js'
-import Landing from '../components/landing/landing.js'
-import Footer from '../components/Footer/Footer.js'
-import InHonor from '../components/InHonor/InHonor.js'
-import Cards from '../components/cards/cards.js'
-import Imagenes from '../components/carousel/Imagenes.js'
-import imagen from '../assets/img/imgExample.png'
+import React from "react";
+import Header from "../components/header/header.js";
+import Landing from "../components/landing/landing.js";
+import Footer from "../components/Footer/Footer.js";
+import InHonor from "../components/InHonor/InHonor.js";
+import Cards from "../components/cards/cards.js";
+import Imagenes from "../components/carousel/Imagenes.js";
+import imagen from "../assets/img/imgExample.png";
 const Home = () => {
   return (
     <div>
-        <Header />
-        <Landing
-         Title={"Barrio seguro"}
-         Subtitle={"BarrioSOS"}
-         Parragraf={'Porque de tal manera amó Dios al mundo que ha dado a su hijo unijenito para que todo el que en Él crea no se pierda más tenga vida eterna'}
-         Boton={"Aportar mi gota"}
-         Img={imagen}
-         Address="/donaciones"
-         />
-        <InHonor />
-        <Cards />
-        <Imagenes />
-        <Footer />
-    </div> 
-  )
-}
+      <Header />
+      <Landing
+        Title={"Gaptos"}
+        Subtitle={"Gotas de amor para todos"}
+        Parragraf={
+          "Porque de tal manera amó Dios al mundo que ha dado a su hijo unijenito para que todo el que en Él crea no se pierda más tenga vida eterna"
+        }
+        Boton={"Aportar mi gota"}
+        Img={imagen}
+        Address="/donaciones"
+      />
+      <InHonor />
+      <Cards />
+      <Imagenes />
+      <Footer />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
