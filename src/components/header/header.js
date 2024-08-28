e8ddc009f8223de5c9db6e4eb451c94ed5646551
Yaswell <featherstone113@gmail.com>
2024-08-28 15:51:59 -0400

    test navigations

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -19,9 +19,9 @@ const hearder = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link href="#">Inicio</Nav.Link>
-            <Nav.Link href="gaptos/#/donaciones">Contribuye</Nav.Link>
+            <Nav.Link route="/donaciones">Contribuye</Nav.Link>
             <NavDropdown title="Nosotros" id="basic-nav-dropdown">
-              <NavDropdown.Item href="gaptos/#/nosotros">
+              <NavDropdown.Item route="/nosotros">
                 ¿Quienes sómos?
               </NavDropdown.Item>
               <NavDropdown.Item href="#/galeria">Galería</NavDropdown.Item>
@@ -29,7 +29,7 @@ const hearder = () => {
                 Proyectos finalizados
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="gaptos/#/transparencia">
+              <NavDropdown.Item href="#/transparencia">
                 Transparencia
               </NavDropdown.Item>
             </NavDropdown>
